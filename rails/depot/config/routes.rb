b4e5149871d4936902d0b2e4d431ca3c00063121
Wesley Li <adwhence@gmail.com>
2014-08-06 22:26:04 +1000

    end of chapter d

--- rails/depot/config/routes.rb
+++ rails/depot/config/routes.rb
@@ -1,11 +1,16 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
