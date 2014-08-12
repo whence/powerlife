a8ee97ad48c87fc52fd14337da46bcbe8d71c64d
Wesley Li <adwhence@gmail.com>
2014-08-12 21:37:20 +1000

    end of tutorial ch.6

--- rails/sample_app/spec/models/user_spec.rb
+++ rails/sample_app/spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to :password_digest }
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
+  it { should respond_to :authenticate }
 
   it { should be_valid }
   
@@ -86,4 +87,33 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe 'with a password too short' do
+    before { @user.password = @user.password_confirmation = 'a' * 5 }
+    it { should be_invalid }
+  end
+
+  describe 'return value of authenticate method' do
+    before { @user.save }
+    let(:found_user) { User.find_by(:email => @user.email) }
+
+    describe 'with valid password' do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe 'with invalid password' do
+      let(:user_for_invalid_password) { found_user.authenticate 'invalid' }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it 'should be saved as all lower-case' do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
