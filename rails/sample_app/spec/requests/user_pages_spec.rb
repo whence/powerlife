9777f3ccf90eca52c2623e28992747b9a0a3cd7c
Wesley Li <adwhence@gmail.com>
2014-08-15 16:51:54 +1000

    end of tutorial ch.8

--- rails/sample_app/spec/requests/user_pages_spec.rb
+++ rails/sample_app/spec/requests/user_pages_spec.rb
@@ -47,8 +47,15 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: email) }
 
+        it { should have_link 'Sign out' }
         it { should have_title user.name }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        describe 'followed by a signout' do
+          before { click_link 'Sign out' }
+
+          it { should have_link 'Sign in' }
+        end
       end
     end
   end
