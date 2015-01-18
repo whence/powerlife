5c1d61a29b1d60afc5b1cb2770b74f7c3692fa8a
Wesley Li <wesley.li@myob.com>
2015-01-18 13:27:33 +1100

    more CardsTest

--- java/powercards/src/main/java/powercards/Cards.java
+++ java/powercards/src/main/java/powercards/Cards.java
@@ -1,5 +1,7 @@
 package powercards;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,10 +17,17 @@ public class Cards {
 
   public static List<Card> moveMany(List<Card> source, List<Card> target, int[] sourceIndexes) {
     List<Card> cards = Arrays.stream(sourceIndexes).mapToObj(source::get).collect(Collectors.toList());
-    int[] invSourceIndexes = inverseIndexes(sourceIndexes, source.size());
-    List<Card> invCards = Arrays.stream(invSourceIndexes).mapToObj(source::get).collect(Collectors.toList());
-    source.clear();
-    source.addAll(invCards);
+
+    if (sourceIndexes.length > source.size() / 2) {
+      int[] invSourceIndexes = inverseIndexes(sortIndexes(sourceIndexes, false), source.size());
+      List<Card> invCards = Arrays.stream(invSourceIndexes).mapToObj(source::get).collect(Collectors.toList());
+      source.clear();
+      source.addAll(invCards);
+    } else {
+      for (int i : sortIndexes(sourceIndexes, true)) {
+        source.remove(i);
+      }
+    }
     target.addAll(cards);
     return cards;
   }
@@ -26,9 +35,18 @@ public class Cards {
   /*
    * Return the inverse of the provided indexes.
    * For example, [1, 2, 4] with cap of 6 will return [0, 3, 5].
-   * Provided indexes must be sorted.
+   * Provided indexes must be sorted ascending.
    */
   public static int[] inverseIndexes(int[] indexes, int capExclusive) {
     return IntStream.range(0, capExclusive).filter(i -> Arrays.binarySearch(indexes, i) < 0).toArray();
+  }
+
+  public static int[] sortIndexes(int[] original, boolean descending) {
+    int[] array = original.clone();
+    Arrays.sort(array);
+    if (descending) {
+      ArrayUtils.reverse(array);
+    }
+    return array;
   }
 }
--- java/powercards/src/test/java/powercards/CardsTest.java
+++ java/powercards/src/test/java/powercards/CardsTest.java
@@ -32,9 +32,39 @@ public class CardsTest {
   }
 
   @Test
-  public void testCardsShouldComparedWithReference() {
+  public void cardsShouldReferenceCompare() {
     assertThat(card1, is(not(card3)));
     assertThat(card2, is(not(card4)));
+  }
+
+  @Test
+  public void arrayListShouldStructuralCompared() {
+    List<Card> a = new ArrayList<>();
+    List<Card> b = new ArrayList<>();
+    a.add(card1);
+    a.add(card2);
+    b.add(card1);
+    b.add(card2);
+    assertThat(a == b, is(false));
+    assertThat(a.equals(b), is(true));
+    assertThat(a, is(b));
+    assertThat(b, is(a));
+
+    List<Card> c = Arrays.asList(card3, card4);
+    List<Card> d = Arrays.asList(card3, card4);
+    assertThat(c == d, is(false));
+    assertThat(c.equals(d), is(true));
+    assertThat(c, is(d));
+    assertThat(d, is(c));
+  }
+
+  @Test
+  public void arrayShouldWorkWithIsOperator() {
+    int[] a = new int[] { 1, 2, 3 };
+    int[] b = new int[] { 1, 2, 3 };
+    assertThat(a == b, is(false));
+    assertThat(a, is(b));
+    assertThat(b, is(a));
   }
 
   @Test
@@ -50,19 +80,57 @@ public class CardsTest {
   }
 
   @Test
-  public void shouldMoveMany() {
+  public void shouldMoveSome() {
     List<Card> source = new ArrayList<>(Arrays.asList(card1, card2, card3));
     List<Card> target = new ArrayList<>(Arrays.asList(card4));
+    int[] sourceIndexes = new int[] { 2, 0 };
 
-    List<Card> cards = Cards.moveMany(source, target, new int[] {0, 2});
+    List<Card> cards = Cards.moveMany(source, target, sourceIndexes);
 
     assertThat(source, is(Arrays.asList(card2)));
-    assertThat(target, is(Arrays.asList(card4, card1, card3)));
-    assertThat(cards, is(Arrays.asList(card1, card3)));
+    assertThat(target, is(Arrays.asList(card4, card3, card1)));
+    assertThat(cards, is(Arrays.asList(card3, card1)));
+    assertThat(sourceIndexes, is(new int[] { 2, 0 }));
+  }
+
+  @Test
+  public void shouldMoveALot() {
+    List<Card> source = new ArrayList<>(Arrays.asList(card1, card2, card3, card4));
+    List<Card> target = new ArrayList<>();
+    int[] sourceIndexes = new int[] { 3, 1 };
+
+    List<Card> cards = Cards.moveMany(source, target, sourceIndexes);
+
+    assertThat(source, is(Arrays.asList(card1, card3)));
+    assertThat(target, is(Arrays.asList(card4, card2)));
+    assertThat(cards, is(Arrays.asList(card4, card2)));
+    assertThat(sourceIndexes, is(new int[] { 3, 1 }));
   }
 
   @Test
   public void shouldInverseIndexes() {
-    assertThat(Cards.inverseIndexes(new int[] { 1, 2, 4 }, 6), is(new int[] { 0, 3, 5 }));
+    int[] original = new int[] { 1, 2, 4 };
+    int[] result = Cards.inverseIndexes(original, 6);
+    assertThat(result, is(new int[] { 0, 3, 5 }));
+    assertThat(original, is(new int[] { 1, 2, 4}));
+    assertThat(result == original, is(false));
+  }
+
+  @Test
+  public void shouldSortIndexesAscending() {
+    int[] original = new int[] { 3, 2, 5, 1 };
+    int[] result = Cards.sortIndexes(original, false);
+    assertThat(result, is(new int[] { 1, 2, 3, 5 }));
+    assertThat(original, is(new int[] { 3, 2, 5, 1 }));
+    assertThat(result == original, is(false));
+  }
+
+  @Test
+  public void shouldSortIndexesDescending() {
+    int[] original = new int[] { 1, 2, 5, 3 };
+    int[] result = Cards.sortIndexes(original, true);
+    assertThat(result, is(new int[] { 5, 3, 2, 1 }));
+    assertThat(original, is(new int[] { 1, 2, 5, 3 }));
+    assertThat(result == original, is(false));
   }
 }
