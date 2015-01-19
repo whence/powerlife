9ed7d1235f9697e9a26baaa6501eb63ad062fa55
Wesley Li <adwhence@gmail.com>
2015-01-19 22:22:05 +1100

    spike on pdfjs

--- python/pdfjs/hello.js
+++ python/pdfjs/hello.js
@@ -1,36 +1,38 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
-
-//
-// See README for overview
-//
-
 'use strict';
 
-//
-// Fetch the PDF document from the URL using promises
-//
 PDFJS.getDocument('helloworld.pdf').then(function(pdf) {
-    // Using promise to fetch the page
     pdf.getPage(1).then(function(page) {
         var scale = 1.5;
         var viewport = page.getViewport(scale);
 
-        //
-        // Prepare canvas using PDF page dimensions
-        //
         var canvas = document.getElementById('the-canvas');
         var context = canvas.getContext('2d');
         canvas.height = viewport.height;
         canvas.width = viewport.width;
 
-        //
-        // Render PDF page into canvas context
-        //
         var renderContext = {
             canvasContext: context,
             viewport: viewport
         };
         page.render(renderContext);
+
+        pdf.getData().then(function(data) {
+            var blob = PDFJS.createBlob(data, 'application/pdf');
+            var blobUrl = URL.createObjectURL(blob);
+        
+            var downloadLink = document.createElement('a');
+            downloadLink.textContent = '[Download]';
+            downloadLink.href = blobUrl;
+            if ('download' in downloadLink) {
+                downloadLink.download = 'helloworld.pdf';
+            }
+            (document.body || document.documentElement).appendChild(downloadLink);
+
+            var printLink = document.createElement('a');
+            printLink.textContent = '[Open in new tab]';
+            printLink.href = blobUrl;
+            printLink.target = '_blank';
+            (document.body || document.documentElement).appendChild(printLink);
+        });
     });
 });
