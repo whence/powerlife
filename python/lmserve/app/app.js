cf93b9837c57e14a8e5e2ad86a4d6906ebdc1910
Wesley Li <adwhence@gmail.com>
2015-04-06 17:34:05 +1000

    lmserve complete list page

--- python/lmserve/app/app.js
+++ python/lmserve/app/app.js
@@ -1,15 +1,58 @@
 $(function() {
-    var Todo = Backbone.Model.extend({
+    var Album = Backbone.Model.extend({
         defaults: function() {
             return {
-                title: "empty todo...",
-                order: 1,
-                done: false
+                photos: []
             };
-        },
-
-        toggle: function() {
-            this.save({done: !this.get("done")});
         }
     });
+
+    var AlbumList = Backbone.Collection.extend({
+        model: Album,
+        
+        url: '/api/albums',
+        
+        parse: function(response) {
+            return response.albums;
+        }
+    });
+
+    var Albums = new AlbumList();
+
+    var AlbumSummaryView = Backbone.View.extend({
+        tagName: 'li',
+        
+        template: _.template($('#album-summary-template').html()),
+
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+    });
+
+    var AppView = Backbone.View.extend({
+        el: $('#lmserve-app'),
+
+        initialize: function() {
+            this.listenTo(Albums, 'add', this.addOne);
+            this.listenTo(Albums, 'reset', this.addAll);
+            Albums.fetch({reset: true});
+        },
+
+        addOne: function(album) {
+            var view = new AlbumSummaryView({model: album});
+            this.$('#album-list').append(view.render().el);
+        },
+
+        addAll: function() {
+            Albums.each(this.addOne, this);
+        }
+    });
+
+    new AppView();
 });
--- python/lmserve/server.py
+++ python/lmserve/server.py
@@ -1,15 +1,33 @@
 from bottle import route, run, static_file
+import os
+
+lms_root = os.path.expanduser('~/lms')
 
 @route('/')
 def index():
     return static_file('index.html', root='app')
 
+@route('/app/<filepath:path>')
+def serve_app(filepath):
+    return static_file(filepath, root='app')
+
 @route('/static/<filepath:path>')
 def serve_static(filepath):
     return static_file(filepath, root='bower_components')
 
-@route('/app/<filepath:path>')
-def serve_app(filepath):
-    return static_file(filepath, root='app')
+@route('/lms/<filepath:path>')
+def serve_lms(filepath):
+    return static_file(filepath, root=lms_root)
+
+def get_albums(root):
+    return [{'name': album, 'path': os.path.join(subdir, album)}
+            for subdir in os.listdir(root) if os.path.isdir(os.path.join(root, subdir))
+            for album in os.listdir(os.path.join(root, subdir)) if os.path.isdir(os.path.join(root, subdir, album))]
+
+albums = get_albums(lms_root)
+
+@route('/api/albums')
+def api_albums():
+    return {'albums': albums}
 
 run(host='localhost', port=8080)
