8e39d6526f6cd8d07d740f595d865ecf1433a671
Wesley Li <wesley.li@myob.com>
2015-05-01 12:10:23 +1000

    split divide into 2 for optimisation

--- scala/powercards_adt/src/main/scala/powercards/Game.scala
+++ scala/powercards_adt/src/main/scala/powercards/Game.scala
@@ -110,9 +110,21 @@ object Dialog {
 }
 
 object Utils {
-  def divide[A](items: Vector[A], indexes: Vector[Int]): (Vector[A], Vector[A]) = {
-    val selected = indexes.map(items(_))
-    val unselected = Range(0, items.length).diff(indexes).map(items(_)).toVector
+  def divides[A](items: Vector[A], indexes: Vector[Int]): (Vector[A], Vector[A]) = indexes match {
+    case Seq() =>
+      (Vector.empty, items)
+    case Seq(i) =>
+      val (x, y) = divide(items, i)
+      (Vector(x), y)
+    case _ =>
+      val selected = indexes.map(items(_))
+      val unselected = Range(0, items.length).diff(indexes).map(items(_)).toVector
+      (selected, unselected)
+  }
+
+  def divide[A](items: Vector[A], index: Int): (A, Vector[A]) = {
+    val selected = items(index)
+    val unselected = items.patch(index, Nil, 1)
     (selected, unselected)
   }
 }
