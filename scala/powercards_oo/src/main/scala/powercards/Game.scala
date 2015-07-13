b5b85347f86539bfeb946f97236060490a43250d
Wesley Li <wesley.li@myob.com>
2015-07-13 12:40:28 +1000

    added stage to game

--- scala/powercards_oo/src/main/scala/powercards/Game.scala
+++ scala/powercards_oo/src/main/scala/powercards/Game.scala
@@ -1,8 +1,15 @@
 package powercards
 
+import powercards.stages.ActionStage
+
 import util.Random.nextInt
 
 class Game(val players: Vector[Player]) {
   private var activePlayerIndex = nextInt(players.length)
   def active = players(activePlayerIndex)
+  private var stage: Stage = ActionStage
+
+  def playOne(): Unit = {
+    stage = stage.play(this)
+  }
 }
