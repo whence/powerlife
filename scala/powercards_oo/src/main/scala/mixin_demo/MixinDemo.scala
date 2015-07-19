77775742633c998d2b380e6b85c994f819947534
Wesley Li <wesley.li@myob.com>
2015-07-20 09:51:25 +1000

    add MixinDemo for scala

---
+++ scala/powercards_oo/src/main/scala/mixin_demo/MixinDemo.scala
@@ -0,0 +1,90 @@
+package mixin_demo
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+trait Card
+
+trait Connection {
+  def send(content: String) = ???
+  def receive(): String = ???
+}
+
+trait Player extends Interactive {
+  def name: String
+  def hand: Vector[Card] = Vector.empty
+}
+
+trait Interactive {
+  def choose(title: String, items: Vector[String]): String
+}
+
+trait ConsoleInteractive extends Interactive {
+  def choose(message: String, items: Vector[String]): String = {
+    println(message)
+    items.foreach(println)
+    val result = io.StdIn.readLine()
+    items.find(_ == result).get
+  }
+}
+
+trait NetworkInteractive extends Interactive {
+  def connection: Connection
+
+  def choose(message: String, items: Vector[String]): String = {
+    connection.send(s"$message\n${items.mkString("\n")}")
+    val result = connection.receive()
+    items.find(_ == result).get
+  }
+}
+
+trait AIInteractive extends Interactive {
+  def hand: Vector[Card]
+  def allLogs: Seq[String]
+
+  def choose(message: String, items: Vector[String]): String = {
+    magic(allLogs, hand)
+  }
+
+  private def magic(logs: Seq[String], hand: Vector[Card]): String = ???
+}
+
+trait AuditingInteractive extends Interactive {
+  def log(thing: String)
+
+  def choose(message: String, items: Vector[String]): String = {
+    val result = super.choose(message, items)
+    log(result)
+    result
+  }
+}
+
+trait GlobalLogging {
+  def logStore: mutable.Buffer[String]
+
+  def log(thing: String) = logStore.append(thing)
+  def allLogs: Seq[String] = logStore.toSeq
+}
+
+object SampleApp {
+  val gameLog = new ArrayBuffer
+
+  val localPlayer = new Player with ConsoleInteractive with AuditingInteractive with GlobalLogging {
+    val name = "local"
+    val logStore = gameLog
+  }
+
+  val networkPlayer = new Player with NetworkInteractive with AuditingInteractive with GlobalLogging {
+    val name = "network"
+    val connection = new Connection {}
+    val logStore = gameLog
+  }
+
+  val aiPlayer = new Player with AIInteractive with AuditingInteractive with GlobalLogging {
+    val name = "ai"
+    val logStore = gameLog
+  }
+
+  val players = Vector(localPlayer, networkPlayer, aiPlayer)
+
+}
