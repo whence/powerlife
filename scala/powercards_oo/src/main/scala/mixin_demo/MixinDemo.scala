ab8b3e6736f0ee34dac09dcaff0b1f438b431641
Wesley Li <wesley.li@myob.com>
2015-07-22 09:25:49 +1000

    simplify mixindemo

--- scala/powercards_oo/src/main/scala/mixin_demo/MixinDemo.scala
+++ scala/powercards_oo/src/main/scala/mixin_demo/MixinDemo.scala
@@ -1,8 +1,5 @@
 package mixin_demo
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-
 trait Card
 
 trait Connection {
@@ -15,25 +12,23 @@ class Player(val name: String) {
 }
 
 trait Interactive {
-  def ask(question: String, items: Vector[String]): String
+  def choose(question: String, items: Vector[String]): String
 }
 
 trait LocalInteractive extends Interactive {
-  def ask(question: String, items: Vector[String]): String = {
+  def choose(question: String, items: Vector[String]): String = {
     println(question)
     items.foreach(println)
-    val result = io.StdIn.readLine()
-    items.find(_ == result).get
+    io.StdIn.readLine()
   }
 }
 
 trait NetworkInteractive extends Interactive {
   def connection: Connection
 
-  def ask(question: String, items: Vector[String]): String = {
+  def choose(question: String, items: Vector[String]): String = {
     connection.send(s"$question\n${items.mkString("\n")}")
-    val result = connection.receive()
-    items.find(_ == result).get
+    connection.receive()
   }
 }
 
@@ -41,50 +36,37 @@ trait AIInteractive extends Interactive {
   def hand: Vector[Card]
   def allLogs: Seq[String]
 
-  def ask(question: String, items: Vector[String]): String = {
-    magic(allLogs, hand)
-  }
-
-  def magic(logs: Seq[String], hand: Vector[Card]): String
+  def choose(question: String, items: Vector[String]): String = ???
 }
 
 trait AuditingInteractive extends Interactive {
-  def log(thing: String)
+  def log(choice: String)
 
-  abstract override def ask(question: String, items: Vector[String]): String = {
-    val result = super.ask(question, items)
-    log(result)
-    result
+  abstract override def choose(question: String, items: Vector[String]): String = {
+    val choice = super.choose(question, items)
+    log(choice)
+    choice
   }
 }
 
-trait GlobalLogging {
-  def logStore: mutable.Buffer[String]
-
-  def log(thing: String) = logStore.append(thing)
-  def allLogs: Seq[String] = logStore.toSeq
+trait LoggingRepository {
+  def log(choice: String) = ???
+  def allLogs: Seq[String] = ???
 }
 
 object SampleApp {
-  val gameLog = new ArrayBuffer[String]
   val dummyConnection = new Connection {
-    def send(content: String) = ???
+    def send(content: String): Unit = ???
     def receive(): String = ???
   }
 
-  val localPlayer = new Player("local") with LocalInteractive with AuditingInteractive with GlobalLogging {
-    val logStore = gameLog
-  }
+  val localPlayer = new Player("local") with LocalInteractive with AuditingInteractive with LoggingRepository
 
-  val networkPlayer = new Player("network") with NetworkInteractive with AuditingInteractive with GlobalLogging {
+  val networkPlayer = new Player("network") with NetworkInteractive with AuditingInteractive with LoggingRepository {
     val connection = dummyConnection
-    val logStore = gameLog
   }
 
-  val aiPlayer = new Player("ai") with AIInteractive with AuditingInteractive with GlobalLogging {
-    val logStore = gameLog
-    def magic(logs: Seq[String], hand: Vector[Card]): String = ???
-  }
+  val aiPlayer = new Player("ai") with AIInteractive with AuditingInteractive with LoggingRepository
 
   val players = Vector(localPlayer, networkPlayer, aiPlayer)
 }
--- scala/powercards_oo/src/test/scala/mixin_demo/MixinDemoSpec.scala
+++ scala/powercards_oo/src/test/scala/mixin_demo/MixinDemoSpec.scala
@@ -3,8 +3,6 @@ package mixin_demo
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{Matchers, FlatSpec}
 
-import scala.collection.mutable.ArrayBuffer
-
 class MixinDemoSpec extends FlatSpec with Matchers with MockFactory {
 
   "NetworkInteractive" should "choose" in {
@@ -15,41 +13,6 @@ class MixinDemoSpec extends FlatSpec with Matchers with MockFactory {
     (interactive.connection.send _).expects("question\nItem1\nItem2")
     (interactive.connection.receive _).expects().returning("Item2")
 
-    interactive.ask("question", Vector("Item1", "Item2")) shouldBe "Item2"
-  }
-
-  "AIPlayer" should "have access to all logs" in {
-    val gameLog = new ArrayBuffer[String]
-
-    val player1 = new Player("player1") with NetworkInteractive with AuditingInteractive with GlobalLogging {
-      val connection = mock[Connection]
-      val logStore = gameLog
-    }
-
-    val player2 = new Player("player2") with NetworkInteractive with AuditingInteractive with GlobalLogging {
-      val connection = mock[Connection]
-      val logStore = gameLog
-    }
-
-    val mockedMagic = mockFunction[Seq[String], Vector[Card], String]
-    val aiPlayer = new Player("ai") with AIInteractive with AuditingInteractive with GlobalLogging {
-      val logStore = gameLog
-      def magic(logs: Seq[String], hand: Vector[Card]): String = mockedMagic(logs, hand)
-    }
-
-    (player1.connection.send _).expects(*)
-    (player2.connection.send _).expects(*)
-
-    inSequence {
-      (player1.connection.receive _).expects().returning("Item2")
-      (player2.connection.receive _).expects().returning("Item3")
-      mockedMagic.expects(Seq("Item2", "Item3"), *).returning("Item1").once()
-      mockedMagic.expects(Seq("Item2", "Item3", "Item1"), *).returning("Item1").once()
-    }
-
-    player1.ask("question", Vector("Item1", "Item2", "Item3")) shouldBe "Item2"
-    player2.ask("question", Vector("Item1", "Item2", "Item3")) shouldBe "Item3"
-    aiPlayer.ask("question", Vector("Item1", "Item2", "Item3")) shouldBe "Item1"
-    aiPlayer.ask("question", Vector("Item1", "Item2", "Item3")) shouldBe "Item1"
+    interactive.choose("question", Vector("Item1", "Item2")) shouldBe "Item2"
   }
 }
