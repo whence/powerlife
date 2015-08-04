d437f08220e9b96cfc8f8431a24bf2cfc70309f2
Wesley Li <wesley.li@myob.com>
2015-08-04 16:32:30 +1000

    move sbt_demo to its own repo

--- scala/sbt_demo/core/src/main/scala/sbt_demo_core/Utils.scala
+++
@@ -1,5 +0,0 @@
-package sbt_demo_core
-
-object Utils {
-  val hello = "hello from Utils"
-}
--- scala/sbt_demo/repositories/src/main/scala/sbt_demo_repositories/HelloRepository.scala
+++
@@ -1,7 +0,0 @@
-package sbt_demo_repositories
-
-import sbt_demo_core.Utils
-
-object HelloRepository {
-  val hello = s"hello from HelloRepository with ${Utils.hello}"
-}
